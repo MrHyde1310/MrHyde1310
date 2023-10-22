90b8d60403c174d97bd8118729e5cb1157e55dd4
Oscar A. Cerdán Córdova <111545878+MrHyde1310@users.noreply.github.com>
2023-10-22 16:08:14 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import streamlit as st
+import pandas as pd
 
-st.title('Mi Dashboard Streamlit')
+st.title('Calculadora de Estadísticas con Streamlit')
 
-# Campo de entrada para rutas de texto
-ruta = st.text_input('Ingrese una ruta de texto', '/ruta/ejemplo')
+# Campo de carga de archivos
+archivo = st.file_uploader('Cargue un archivo Excel', type=['xls', 'xlsx'])
 
-st.write('Ruta ingresada:', ruta)
+if archivo is not None:
+    # Leer el archivo Excel y cargar los datos en un DataFrame de pandas
+    df = pd.read_excel(archivo, sheet_name='Datos')
+
+    st.subheader('Datos del archivo:')
+    st.write(df)
+
+    # Seleccionar la columna para calcular estadísticas
+    columna = st.selectbox('Seleccione una columna:', df.columns)
+
+    st.subheader('Resultados de estadísticas:')
+    st.write(f'Media: {df[columna].mean()}')
+    st.write(f'Mediana: {df[columna].median()}')
+    st.write(f'Desviación Estándar: {df[columna].std()}')
